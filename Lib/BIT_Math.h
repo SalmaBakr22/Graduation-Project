3e951a1d4b544dd0c8a67932d8f69744799ee03f
ALIREDA5 <alireda753951@gmail.com>
2024-01-25 21:25:25 +0200

    p

---
+++ Lib/BIT_Math.h
@@ -0,0 +1,16 @@
+/******************************************/
+/* Desc    : BIT_Math Header file         */
+/* Author  : Salma Bakr 			      */
+/* Date    : 5 Aug 2023 				  */
+/* Version : 1.0v                         */
+/******************************************/
+
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+#define SET_BIT(Reg , bitNO)            Reg |=  (1 << (bitNO))
+#define CLEAR_BIT(Reg , bitNO)          Reg &= ~(1 << (bitNO))
+#define TGL_BIT(Reg , bitNO)            Reg ^=  (1 << (bitNO))
+#define GET_BIT(Reg , bitNO)            ((Reg) >> (bitNO)) & 1
+
+#endif /*End guard*/
---
+++ Lib/STD_Types.h
@@ -0,0 +1,28 @@
+/******************************************/
+/* Desc    : Standard types header file   */
+/* Author  : Salma Bakr 			      */
+/* Date    : 4 Aug 2023 				  */
+/* Version : 1.0v                         */
+/******************************************/
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef signed char              sint8  ;
+typedef unsigned char            uint8  ;
+typedef signed short int         sint16 ;
+typedef unsigned short int       uint16 ;
+typedef signed long int          sint32 ;
+typedef unsigned long int        uint32 ;
+typedef float                    float32;
+typedef double                   float64;
+
+typedef enum 
+{
+	E_OK,
+	E_NOK,
+	PARM_POT_OF_RANGE,
+	PTR_IS_NULL
+}tenuErrorStatus;
+
+#endif   /*End Guard*/
